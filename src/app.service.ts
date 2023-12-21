91201bf69317c1035372eae6c9f9cef82be65257
ohansck <ohansck@gmail.com>
2023-12-21 15:21:04 +0100

    updated CICD algo

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Cloud In December CICD v1.0';
+    return 'Cloud In December CICD v1.5';
   }
 
   checkHealth(): string {
