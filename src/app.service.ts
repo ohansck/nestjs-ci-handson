94c600a6343f5264f9e3846a62f68b0bf31c9411
ohansck <ohansck@gmail.com>
2023-12-24 08:16:07 +0100

    mod: algorithim

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,11 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-<<<<<<< HEAD
-    return 'Cloud In December CICD v1.5';
-=======
     return 'Cloud In December CICD v2.0';
->>>>>>> main
   }
 
   checkHealth(): string {
